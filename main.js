c69aed0a4960f402606ad5ef44f9cdd8b6d5125a
Gangbros777 <161280210+Gangbros777@users.noreply.github.com>
2024-07-29 22:01:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let firstNumber = +prompt("Birinchi sonni kiriting")
+let secondNumber = +prompt("Ikkinchi sonni kiriting")
+
+if(firstNumber > secondNumber){
+    alert(`${firstNumber} ikkinchi sondan kotta`)
+}else{
+    alert(`${secondNumber} birinchi sondan katta`)
+}
